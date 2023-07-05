9fc9326b616084e1de764c7e22ccb302478d48af
sunnyrithvik <sribhashyamrithvik@gmail.com>
2023-07-05 17:02:43 +0530

    added context api

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={["terrible", "bad", "okay", "good", "amazing"]}
+    />
+    <StarRating size={24} color="red" defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
