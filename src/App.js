4a6cb8b71ca0a90451bf99cded73db46313dfb04
sunnyrithvik <sribhashyamrithvik@gmail.com>
2023-08-06 22:27:12 +0530

    modified document title

--- src/App.js
+++ src/App.js
@@ -298,6 +298,14 @@ function MovieDetail({ selectedId, onCloseMovie, onAddWatched, watched }) {
     [selectedId]
   );
 
+  useEffect(
+    function () {
+      if (!title) return;
+      document.title = `Movie: ${title}`;
+    },
+    [title]
+  );
+
   return (
     <div className="details">
       {isLoading ? (
