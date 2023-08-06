b4c7e69aca3aa14045104f232fe667722e26e5bd
sunnyrithvik <sribhashyamrithvik@gmail.com>
2023-08-06 23:05:09 +0530

    listening to keypress

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ const average = (arr) =>
 const KEY = "7479dfb6";
 
 export default function App() {
-  const [query, setQuery] = useState("inception");
+  const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -101,8 +101,8 @@ export default function App() {
           // console.log(data);
           setError("");
         } catch (err) {
-          console.error(err.message);
           if (err.name !== "AbortError") {
+            console.log(err.message);
             setError(err.message);
           }
         } finally {
@@ -115,6 +115,7 @@ export default function App() {
         return;
       }
 
+      handleCloseMovie();
       fetchMovies();
 
       return function () {
@@ -292,6 +293,23 @@ function MovieDetail({ selectedId, onCloseMovie, onAddWatched, watched }) {
     onAddWatched(newWatchedMovie);
     onCloseMovie();
   }
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (e.code === "Escape") {
+          onCloseMovie();
+          // console.log("closing");
+        }
+      }
+      document.addEventListener("keydown", callback);
+      //cleanup function
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [onCloseMovie]
+  );
 
   useEffect(
     function () {
